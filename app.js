89b291a3454e79cf7625455958c58b04109f1d52
DevDoBim <zmdkdna2@naver.com>
2023-06-27 16:05:46 +0900

    middlewares create

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const { sequelize } = require('./models/index.js');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
-async function main() {
-  // model을 이용해 데이터베이스에 테이블을 삭제 후 생성합니다.
-  await sequelize.sync({ force: true });
-}
+const usersRouter = require("./routes/users.route");
 
-main();
+const app = express();
+const PORT = 3018;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', [usersRouter]);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+})
